8a8cd419cea990cfd91f70fae2d76bf75b806cc2
Finnshuntaro <Finnshuntaro@gmail.com>
2025-03-25 14:19:24 +1100

    PR04 Progress

---
+++ Base Project/Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    https://www.youtube.com/watch?v=ONlMEZs9Rgw
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
