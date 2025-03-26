e2de0e3f480de90b19ad485b596f511a557ac5e3
Finnshuntaro <Finnshuntaro@gmail.com>
2025-03-26 21:42:06 +1100

    Update Footsteps.cs

--- Base Project/Assets/Scripts/FinnScripts/Footsteps.cs
+++ Base Project/Assets/Scripts/FinnScripts/Footsteps.cs
@@ -6,7 +6,7 @@ public class Footsteps : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.S) || (Input.GetKey(KeyCode.S)))))
+        if (Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.S) || (Input.GetKey(KeyCode.D)))))
         {
             footstepsSound.enabled = true;
         }
