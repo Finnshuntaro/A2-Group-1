7e791cbae200582df3bb2ab78eee8b95ceeee893
DeadWOrld23 <dead.world233@gmail.com>
2025-03-28 15:44:15 +1100

    Player And Camera Movement
    made very simple scene, focused on player movement and camera movement. I also added quick player cursor.

---
+++ Base Project/Assets/Scripts/SamuelScripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1f;
+
+    
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded; 
+    
+    void Update()
+    {
+        // Makes small sphere to check to see if player is touching ground, it will ignore anything not layered with 'Ground' layer.
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        /* Use to capture keyboard or controller input for movement  
+         * you can assign what Horizontal and Vertical are key binded with in input manager in project settings.*/
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Move across the x and z axis freely
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity);
+
+        
+    }
+}
