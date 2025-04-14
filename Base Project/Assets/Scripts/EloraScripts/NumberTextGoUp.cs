95e43e238ee869a3c23a22890107fde372b37596
ebeeeeeee <elorabosma1@gmail.com>
2025-04-14 12:10:10 +1000

    ui code

---
+++ Base Project/Assets/Scripts/EloraScripts/NumberTextGoUp.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using TMPro;
+
+public class NumberTextGoUp : MonoBehaviour
+{
+    public TextMeshProUGUI numberText;
+    int counter;
+
+    public void ButtonPressed()
+    {
+        counter++;
+        numberText.text = counter + "";
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
