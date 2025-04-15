a4197b921e8410fed8a13956bbcdd197f730fdbf
DeadWOrld23 <dead.world233@gmail.com>
2025-04-15 12:56:53 +1000

    Dialogue Text Speed Done
    Made it so that the dialogue is typed out one character at a time depending on the speed float. This doesnt happen to NPC name.

--- Base Project/Assets/Scripts/SamuelScripts/DialogueManger.cs
+++ Base Project/Assets/Scripts/SamuelScripts/DialogueManger.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;  // Use the TMPro namespace for TextMeshPro components
@@ -8,8 +9,11 @@ public class DialogueManager : MonoBehaviour
     public GameObject dialoguePanel;   // The panel that will show the dialogue UI
     public TextMeshProUGUI nameText;   // The TextMeshProUGUI component for displaying the NPC's name
     public TextMeshProUGUI dialogueText; // The TextMeshProUGUI component for displaying the dialogue
+    public float textSpeed; // How fast the text is typed out in the dialogue box
+
     private Queue<string> sentences;   // Queue to hold all the sentences of dialogue
     private bool isDialogueActive = false;  // Flag to check if dialogue is active
+    private Coroutine typingCoroutine; // Keeps track of the currently running typing effect so it can be stopped if needed
 
     void Start()
     {
@@ -37,9 +41,19 @@ public class DialogueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isDialogueActive && Input.GetKeyDown(KeyCode.F))  // If dialogue is active, and the player presses F
+        if (isDialogueActive && Input.GetKeyDown(KeyCode.F))
         {
-            DisplayNextSentence();  // Display the next sentence in the dialogue
+            if (typingCoroutine != null)
+            {
+                // Instantly finish typing the current line
+                StopCoroutine(typingCoroutine);
+                dialogueText.text = sentences.Peek(); // Show whole sentence
+                typingCoroutine = null;
+            }
+            else
+            {
+                DisplayNextSentence();
+            }
         }
     }
 
@@ -53,7 +67,11 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();  // Get the next sentence from the queue
-        dialogueText.text = sentence;           // Display it in the dialogue box
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+        typingCoroutine = StartCoroutine(TypeSentence(sentence));           // Display it in the dialogue box
     }
 
     // End the dialogue when all sentences are displayed
@@ -61,5 +79,18 @@ public class DialogueManager : MonoBehaviour
     {
         isDialogueActive = false;
         dialoguePanel.SetActive(false);  // Hide the dialogue panel
+    }
+
+    private IEnumerator TypeSentence(string sentence) // 
+    {
+        dialogueText.text = "";
+
+        foreach (char letter in sentence)
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(textSpeed);
+        }
+
+        typingCoroutine = null; // Reset when done
     }
 }
